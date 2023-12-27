9e336e959b115cd5c492bc260c786365fec4984e
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-28 02:39:12 +0300

    get 20 rubrics by date

--- backend/api/actions/rubrics.py
+++ backend/api/actions/rubrics.py
@@ -28,23 +28,50 @@ async def _get_all_rubrics_by_text(text: str, pg_session: AsyncSession, es_sessi
     res = await es_session.search(
         index=INDEX_RUBRICS,
         query={"match": {"text": {"query": text}}},
-        size=20
+        size=21,
+        scroll=settings.SCROLL_TIME
     )
     rubrics_data = res.get("hits").get("hits")
     if rubrics_data:
-        id_rubrics = [
-            rubric.get("_source").get("id")
-            for rubric in rubrics_data
-        ]
-        rubric_dal = RubricDAL(pg_session)
-        all_rubrics = sorted([await rubric_dal.get_rubric_by_id(id) for id in id_rubrics],
-                             key=lambda rubric: rubric.created_date)
-        return [ShowRubric(id=rubric.id, text=rubric.text, rubrics=rubric.rubrics, created_date=rubric.created_date)
-                for rubric in all_rubrics]
+        if len(rubrics_data) <= 20:
+            id_rubrics = [
+                rubric.get("_source").get("id")
+                for rubric in rubrics_data
+            ]
+            rubric_dal = RubricDAL(pg_session)
+            all_rubrics = sorted([await rubric_dal.get_rubric_by_id(id) for id in id_rubrics],
+                                 key=lambda rubric: rubric.created_date)
+            return [ShowRubric(id=rubric.id, text=rubric.text, rubrics=rubric.rubrics, created_date=rubric.created_date)
+                    for rubric in all_rubrics]
+        else:
+            scroll_id = res.get("_scroll_id")
+            all_rubrics = []
+            rubric_dal = RubricDAL(pg_session)
+            first_rubric_id = rubrics_data[0].get("_source").get("id")
+            max_rubric = await rubric_dal.get_rubric_by_id(first_rubric_id)
+            while rubrics_data:
+                for rubric_data in rubrics_data[1:]:
+                    rubric_id = rubric_data.get("_source").get("id")
+                    rubric = await rubric_dal.get_rubric_by_id(rubric_id)
+                    if rubric.created_date > max_rubric.created_date:
+                        all_rubrics.append(max_rubric)
+                        max_rubric = rubric
+                    else:
+                        all_rubrics.append(rubric)
+                res = await es_session.options(ignore_status=400).scroll(
+                    scroll_id=scroll_id, scroll=settings.SCROLL_TIME
+                )
+                if res.meta.status == 200:
+                    rubrics_data = res.get("hits").get("hits")
+
+            all_rubrics = sorted(all_rubrics,
+                                 key=lambda rubric: rubric.created_date)
+            return [
+                ShowRubric(id=rubric.id, text=rubric.text, rubrics=rubric.rubrics, created_date=rubric.created_date)
+                for rubric in all_rubrics][:20]
 
 
 async def _delete_rubric_by_id(id: int, pg_session: AsyncSession) -> DeletedRubric | None:
-
     async with pg_session.begin():
         rubric_dal = RubricDAL(pg_session)
         deleted_id = await rubric_dal.delete_rubric_by_id(id=id)
