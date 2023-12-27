5145f13465992b2b159dfa035565fb176daf2f47
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-28 02:05:32 +0300

    add router rubrics

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 from fastapi import FastAPI
 
+from api.rubrics.handlers import rubrics_router
+
 app = FastAPI(description="Rubrics")
 main_api_router = APIRouter()
+main_api_router.include_router(rubrics_router, prefix="/rubrics", tags=["rubrics"])
+app.include_router(main_api_router)
