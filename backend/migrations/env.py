9f1fec5cf835f109b782b8238f4d762e9bb6e24c
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-27 21:12:55 +0300

    add model to env alembic

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -23,6 +23,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from db.base import Base
+from db.postgres.models import Rubric
 
 target_metadata = Base.metadata
 
