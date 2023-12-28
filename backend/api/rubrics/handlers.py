d71f537838dc865863e133b3a289ac486ee70a7f
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-28 12:33:13 +0300

    change handlers with new actions

--- backend/api/rubrics/handlers.py
+++ backend/api/rubrics/handlers.py
@@ -7,19 +7,22 @@ from fastapi import HTTPException
 from fastapi import status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.actions.rubrics import _create_rubric, _get_all_rubrics_by_text, _delete_rubric_by_id
+from api.actions.rubrics import _create_rubric, _get_20_rubrics_by_text, _delete_rubric_by_id
 from api.rubrics.schemas import ShowRubric, CreateRubric, DeletedRubric
-from db.elasticsearch.session_es import get_db_es
-from db.postgres.session_pg import get_db_pg
+from db.es.session_es import get_db_es
+from db.pg.session_pg import get_db_pg
+
+
 rubrics_router = APIRouter()
 
 
 @rubrics_router.post("/")
 async def create_rubric(
         body: CreateRubric,
-        pg_session: AsyncSession = Depends(get_db_pg)
+        pg_session: AsyncSession = Depends(get_db_pg),
+        es_session: AsyncElasticsearch = Depends(get_db_es)
 ):
-    res = await _create_rubric(rubric=body, pg_session=pg_session)
+    res = await _create_rubric(rubric=body, pg_session=pg_session, es_session=es_session)
     if res is None:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -31,10 +34,10 @@ async def create_rubric(
 @rubrics_router.get("/", response_model=List[ShowRubric])
 async def get_rubrics_by_text(
         text: str,
-        es_session: AsyncElasticsearch = Depends(get_db_es),
-        pg_session: AsyncSession = Depends(get_db_pg)
+        pg_session: AsyncSession = Depends(get_db_pg),
+        es_session: AsyncElasticsearch = Depends(get_db_es)
 ) -> List[ShowRubric]:
-    res = await _get_all_rubrics_by_text(text=text, es_session=es_session, pg_session=pg_session)
+    res = await _get_20_rubrics_by_text(text=text, pg_session=pg_session, es_session=es_session)
     if res is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -46,9 +49,10 @@ async def get_rubrics_by_text(
 @rubrics_router.delete("/", response_model=DeletedRubric)
 async def delete_rubric_by_id(
         id: int,
-        pg_session: AsyncSession = Depends(get_db_pg)
+        pg_session: AsyncSession = Depends(get_db_pg),
+        es_session: AsyncElasticsearch = Depends(get_db_es)
 ) -> DeletedRubric:
-    res = await _delete_rubric_by_id(id=id, pg_session=pg_session)
+    res = await _delete_rubric_by_id(id=id, pg_session=pg_session, es_session=es_session)
     if res is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
