8c43c48c2bdbdf8fbca51f42b6405819ac80055e
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-29 01:44:14 +0300

    refactoring code with the help of lint

--- backend/tests/test_api_rubrics/test_delete_rubric_handler.py
+++ backend/tests/test_api_rubrics/test_delete_rubric_handler.py
@@ -1,8 +1,6 @@
 import datetime
-import json
 
 import pytest
-
 from tests.conftest import create_rubric
 
 
@@ -10,31 +8,35 @@ from tests.conftest import create_rubric
     "rubric_data, expected_status_code",
     [
         (
-                {
-                    "text": "Розыгрыш на кепки!",
-                    "rubrics": ["vk.com/1", "vk.com/2", "vk.com/3"],
-                },
-                200,
+            {
+                "text": "Розыгрыш на кепки!",
+                "rubrics": ["vk.com/1", "vk.com/2", "vk.com/3"],
+            },
+            200,
         ),
         (
-                {
-                    "text": "Розыгрыш на худи!",
-                    "rubrics": ["vk.com/1", "vk.com/2", "vk.com/3"],
-                },
-                200,
+            {
+                "text": "Розыгрыш на худи!",
+                "rubrics": ["vk.com/1", "vk.com/2", "vk.com/3"],
+            },
+            200,
         ),
     ],
 )
 async def test_delete_rubric_handler(
-        client,
-        asyncpg_pool,
-        test_async_client_es,
-        rubric_data,
-        expected_status_code,
+    client,
+    asyncpg_pool,
+    test_async_client_es,
+    rubric_data,
+    expected_status_code,
 ):
-    rubric_id = await create_rubric(asyncpg_pool=asyncpg_pool, rubric_id=10001,
-                                    rubrics=rubric_data['rubrics'], text=rubric_data['text'],
-                                    created_date=datetime.datetime.utcnow())
+    rubric_id = await create_rubric(
+        asyncpg_pool=asyncpg_pool,
+        rubric_id=10001,
+        rubrics=rubric_data["rubrics"],
+        text=rubric_data["text"],
+        created_date=datetime.datetime.utcnow(),
+    )
 
     response = await client.delete(
         f"/rubrics/{rubric_id}",
@@ -45,7 +47,7 @@ async def test_delete_rubric_handler(
 
 
 async def test_delete_rubric_handler_not_found(
-        client,
+    client,
 ):
     response = await client.delete(
         "/rubrics/1242352",
