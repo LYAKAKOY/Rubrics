846b38ee2414f1a616b0d24f242a3f0084a7bf37
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-28 12:30:51 +0300

    moved to es directory

--- backend/db/es/migrations.py
+++ backend/db/es/migrations.py
@@ -1,15 +1,20 @@
 import asyncio
 import os
+from logging import getLogger
+
 from elasticsearch import AsyncElasticsearch
 from indexes import all_indexes
 
+logger = getLogger(__name__)
+
 es_database_url = f"http://{os.environ.get('ES_DATABASE')}:{os.environ.get('ES_PORT')}"
+
 DEFAULT_SETTINGS_INDEXES = {"number_of_shards": 3, "number_of_replicas": 2}
 
 
 async def create_index(name_index: str, mappings: dict):
     elastic_client = AsyncElasticsearch(hosts=es_database_url)
-    await elastic_client.options(ignore_status=400).indices.create(
+    await elastic_client.indices.create(
         index=name_index, mappings=mappings, settings=DEFAULT_SETTINGS_INDEXES
     )
     await elastic_client.close()
@@ -21,7 +26,9 @@ async def migrations():
         asyncio.create_task(create_index(name_index=index, mappings=mappings))
         for index, mappings in all_indexes.items()
     ]
-    await asyncio.gather(*tasks, return_exceptions=False)
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    for res in results:
+        logger.info(res)
 
 if __name__ == "__main__":
     asyncio.run(migrations())
